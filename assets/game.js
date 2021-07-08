16c513240cc6b7a88abdbdf2dcde66d310c63618
Damhanlk <damhan2611@gmail.com>
2021-07-08 15:15:28 +0000

    add second html file 'game.html' for game screen

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,2 @@
+// Music functions //
 
-function togglePlay() {
-    $("#sound i").toggleClass("fa-volume-up fa-volume-mute");
-    let audio = document.getElementById("main-menu-music");
-    return audio.paused ? audio.play() : audio.pause();
-}
